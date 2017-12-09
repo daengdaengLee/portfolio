c626e42f36f974894d2ad2e4331d041c7285ed99
KunhoLee <gunho1020@gmail.com>
2017-12-09 21:24:04 +0900

    Vuex 설정 및 sns contacts 관련 state 작성

---
+++ src/store/state.js
@@ -0,0 +1,8 @@
+export default {
+  snsContacts: {
+    githubAddress: 'https://github.com/daengdaengLee',
+    facebookAddress: 'https://www.facebook.com/gunho.lee.566',
+    codepenAddress: 'https://codepen.io/daengdaengLee/',
+    mailAddress: 'gunho1020@gmail.com',
+  },
+};
