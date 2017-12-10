c1dbc03920eca0f2b807c662f210ae90ff92e200
KunhoLee <gunho1020@gmail.com>
2017-12-10 20:05:21 +0900

    image를 github issue를 통해 호스팅하는 방식으로 변경하고

--- src/store/state.js
+++ src/store/state.js
@@ -1,11 +1,4 @@
-const avatarImgUrl = require('../assets/avatarimage.jpg');
-const thumbImgUrl1 = require('../assets/thumbnails/01.jpg');
-const thumbImgUrl2 = require('../assets/thumbnails/02.jpg');
-const thumbImgUrl3 = require('../assets/thumbnails/03.jpg');
-const thumbImgUrl4 = require('../assets/thumbnails/04.jpg');
-const thumbImgUrl5 = require('../assets/thumbnails/05.jpg');
-const thumbImgUrl6 = require('../assets/thumbnails/06.jpg');
-const thumbImgUrl7 = require('../assets/thumbnails/07.jpg');
+// const thumbImgUrl7 = require('../assets/thumbnails/07.jpg');
 
 export default {
   snsContacts: {
@@ -14,13 +7,13 @@ export default {
     codepenAddress: 'https://codepen.io/daengdaengLee/',
     mailAddress: 'gunho1020@gmail.com',
   },
-  avatarImgUrl,
+  avatarImgUrl: 'https://user-images.githubusercontent.com/30795415/33804221-adf180e4-dde3-11e7-8668-d5d0e3e744e5.jpg',
   thumbnailsInfo: [
     [
       {
         id: 'thumb-1',
         linkUrl: 'https://daengdaenglee.github.io/calculator_with_vuejs/',
-        imgUrl: thumbImgUrl1,
+        imgUrl: 'https://user-images.githubusercontent.com/30795415/33804268-a964c760-dde4-11e7-9d0c-53d0eb5ea284.jpg',
         altMessage: 'web calculator thumbnail image',
         headingText: 'Web Calculator',
         descripttionText: 'html, css, javascript, vue.js',
@@ -28,7 +21,7 @@ export default {
       {
         id: 'thumb-2',
         linkUrl: 'https://www.google.com',
-        imgUrl: thumbImgUrl2,
+        imgUrl: 'https://user-images.githubusercontent.com/30795415/33804274-c22524a2-dde4-11e7-903b-8d59bf9d5abe.jpg',
         altMessage: 'thumbnail image 2',
         headingText: 'Heading Text 2',
         descripttionText: 'Description Text 2',
@@ -38,7 +31,7 @@ export default {
       {
         id: 'thumb-3',
         linkUrl: 'https://www.google.com',
-        imgUrl: thumbImgUrl3,
+        imgUrl: 'https://user-images.githubusercontent.com/30795415/33804276-e083511c-dde4-11e7-9ee3-ccdce5f82ba4.jpg',
         altMessage: 'thumbnail image 3',
         headingText: 'Heading Text 3',
         descripttionText: 'Description Text 3',
@@ -46,7 +39,7 @@ export default {
       {
         id: 'thumb-4',
         linkUrl: 'https://www.google.com',
-        imgUrl: thumbImgUrl4,
+        imgUrl: 'https://user-images.githubusercontent.com/30795415/33804280-f99575d6-dde4-11e7-8dbf-219e38041cde.jpg',
         altMessage: 'thumbnail image 4',
         headingText: 'Heading Text 4',
         descripttionText: 'Description Text 4',
@@ -54,7 +47,7 @@ export default {
       {
         id: 'thumb-5',
         linkUrl: 'https://www.google.com',
-        imgUrl: thumbImgUrl5,
+        imgUrl: 'https://user-images.githubusercontent.com/30795415/33804285-0e3d1d72-dde5-11e7-8e20-a64b8a32c1e7.jpg',
         altMessage: 'thumbnail image 5',
         headingText: 'Heading Text 5',
         descripttionText: 'Description Text 5',
@@ -64,7 +57,7 @@ export default {
       {
         id: 'thumb-6',
         linkUrl: 'https://www.google.com',
-        imgUrl: thumbImgUrl6,
+        imgUrl: 'https://user-images.githubusercontent.com/30795415/33804288-22983144-dde5-11e7-8ca0-d78c13107fbf.jpg',
         altMessage: 'thumbnail image 6',
         headingText: 'Heading Text 6',
         descripttionText: 'Description Text 6',
@@ -72,7 +65,7 @@ export default {
       {
         id: 'thumb-7',
         linkUrl: 'https://www.google.com',
-        imgUrl: thumbImgUrl7,
+        imgUrl: 'https://user-images.githubusercontent.com/30795415/33804294-3e1bede8-dde5-11e7-9087-1d9959e74aed.jpg',
         altMessage: 'thumbnail image 7',
         headingText: 'Heading Text 7',
         descripttionText: 'Description Text 7',
