786d83f15e5ac98e62d81c2cee072c0ed3784b9c
KunhoLee <gunho1020@gmail.com>
2017-12-10 23:32:26 +0900

    github page 주소 변경에 따른 라우트 path 변경

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/',
+      path: '/portfolio',
       name: 'PortfolioProjects',
       component: PortfolioProjects,
     },
