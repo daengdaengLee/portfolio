a02e8a0d1c7cca0e60219f4db06d4887439cf637
KunhoLee <gunho1020@gmail.com>
2017-12-25 23:17:46 +0900

    Vuex에서 actions 제거

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,11 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import state from './state';
-import actions from './actions';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state,
-  actions,
 });
 
 export default store;
