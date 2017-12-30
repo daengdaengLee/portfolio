a56ce1f5569de1fad02310d5da6cfe190dc46d1d
KunhoLee <gunho1020@gmail.com>
2017-12-30 21:34:22 +0900

    Vuex state.js에 movie app react 프로젝트의 heading text 업데이트

--- src/store/state.js
+++ src/store/state.js
@@ -31,7 +31,7 @@ export default {
         linkUrl: 'https://daengdaenglee.github.io/movie_app_react/',
         imgUrl: 'https://user-images.githubusercontent.com/30795415/33804325-d055f294-dde5-11e7-8ac1-2b2a7254aa4c.jpg',
         altMessage: 'movie app react thumbnail image',
-        headingText: 'Heading Text 3',
+        headingText: 'Movie App React',
         descripttionText: 'Description Text 3',
       },
       {
