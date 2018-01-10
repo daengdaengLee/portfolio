5e6eabf24f7c1be55d9d392e976e1cd9d029a6f8
KunhoLee <gunho1020@gmail.com>
2018-01-10 10:03:22 +0900

    snsContact 스타일 리팩토링

--- src/components/PortfolioContacts.vue
+++ src/components/PortfolioContacts.vue
@@ -1,10 +1,28 @@
 <template>
-  <ul class="PortfolioContacts">
-    <li><a :href="snsContacts.githubAddress" class="icon-github" target="_blank"><span class="label">Github</span></a></li>
-    <li><a :href="snsContacts.facebookAddress" class="icon-facebook" target="_blank"><span class="label">Facebook</span></a></li>
-    <li><a :href="snsContacts.codepenAddress" class="icon-codepen" target="_blank"><span class="label">Codepen</span></a></li>
-    <li><a :href="'mailto: ' + snsContacts.mailAddress" class="icon-mail" target="_blank"><span class="label">Email</span></a></li>
-  </ul>
+  <div class="d-flex justify-content-center">
+    <ul class="PortfolioContacts list-inline">
+      <li class="list-inline-item IconMargin">
+        <a :href="snsContacts.githubAddress" class="icon-github" target="_blank">
+          <span class="label">Github</span>
+        </a>
+      </li>
+      <li class="list-inline-item IconMargin">
+        <a :href="snsContacts.facebookAddress" class="icon-facebook" target="_blank">
+          <span class="label">Facebook</span>
+        </a>
+      </li>
+      <li class="list-inline-item IconMargin">
+        <a :href="snsContacts.codepenAddress" class="icon-codepen" target="_blank">
+          <span class="label">Codepen</span>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a :href="'mailto: ' + snsContacts.mailAddress" class="icon-mail" target="_blank">
+          <span class="label">Email</span>
+        </a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -19,25 +37,24 @@ export default {
 <style scoped>
 .PortfolioContacts {
   margin: 2rem 0 2rem 0;
-  text-align: center;
 }
-.PortfolioContacts li {
-  display: inline-block;
+.PortfolioContacts .IconMargin {
+  margin-right: 1rem;
+}
+.PortfolioContacts .list-inline-item {
   border: 1px solid darkgray;
   border-radius: 100%;
   width: 2rem;
   height: 2rem;
-  margin-right: 0.7rem;
-  margin-left:  0.7rem;
 }
-.PortfolioContacts li:hover {
+.PortfolioContacts .list-inline-item:hover {
   background-color: darkgray;
   cursor: pointer;
 }
-.PortfolioContacts li .label {
+.PortfolioContacts .list-inline-item .label {
   display: none;
 }
-.PortfolioContacts li a {
+.PortfolioContacts .list-inline-item a {
   display: inline-flex;
   width: 100%;
   height: 100%;
@@ -46,7 +63,7 @@ export default {
   color: gray;
   text-decoration: none;
 }
-.PortfolioContacts li a:hover {
+.PortfolioContacts .list-inline-item a:hover {
   color: white;
 }
 </style>
