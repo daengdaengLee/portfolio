812d902496047d9bb1b2691afdb695c00701615b
KunhoLee <gunho1020@gmail.com>
2018-01-11 00:53:53 +0900

    AboutMe 컴포넌트 생성 및 등록

---
+++ src/components/PortfolioAbout/AboutMe/AboutMe.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="AboutMe">
+    <h3 class="HeadingText">About Me</h3>
+    <p class="MainText">안녕하세요. 저는 웹 개발 꿈나무입니다.</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AboutMe',
+};
+</script>
+
+<style scoped>
+.AboutMe .HeadingText {
+  font-size: 1.4rem;
+  font-weight: 300;
+  color: gray;
+}
+.AboutMe .MainText {
+  font-size: 1.2rem;
+  font-weight: 300;
+}
+</style>
--- src/components/PortfolioAbout/PortfolioAbout.vue
+++ src/components/PortfolioAbout/PortfolioAbout.vue
@@ -1,12 +1,18 @@
 <template>
   <section class="PortfolioAbout">
     <h2 class="HeadingText text-center py-3">About</h2>
+    <about-me />
   </section>
 </template>
 
 <script>
+import AboutMe from './AboutMe/AboutMe';
+
 export default {
   name: 'PortflioAbout',
+  components: {
+    AboutMe,
+  },
 };
 </script>
 
