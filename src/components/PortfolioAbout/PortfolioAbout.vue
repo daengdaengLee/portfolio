c295b2afd53dc66a44b0438dd173fc8ca90650a3
KunhoLee <gunho1020@gmail.com>
2018-01-11 01:04:53 +0900

    Skills 컴포넌트 생성 및 등록

--- src/components/PortfolioAbout/PortfolioAbout.vue
+++ src/components/PortfolioAbout/PortfolioAbout.vue
@@ -1,17 +1,20 @@
 <template>
   <section class="PortfolioAbout">
     <h2 class="HeadingText text-center py-3">About</h2>
-    <about-me />
+    <about-me class="mb-5" />
+    <skills />
   </section>
 </template>
 
 <script>
 import AboutMe from './AboutMe/AboutMe';
+import Skills from './Skills/Skills';
 
 export default {
   name: 'PortflioAbout',
   components: {
     AboutMe,
+    Skills,
   },
 };
 </script>
---
+++ src/components/PortfolioAbout/Skills/Skills.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="Skills">
+    <h3 class="HeadingText my-4">Skills</h3>
+    <p class="MainText">my skills...</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+};
+</script>
+
+<style scoped>
+.Skills .HeadingText {
+  font-size: 1.4rem;
+  font-weight: 300;
+  color: gray;
+}
+.Skills .MainText {
+  font-size: 1.2rem;
+  font-weight: 300;
+}
+</style>
