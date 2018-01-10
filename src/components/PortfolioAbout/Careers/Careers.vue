6e9fa9f813c0c042263ccd8e1688024735af9c61
KunhoLee <gunho1020@gmail.com>
2018-01-11 01:06:51 +0900

    Careers 컴포넌트 생성 및 등록

---
+++ src/components/PortfolioAbout/Careers/Careers.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="Careers">
+    <h3 class="HeadingText my-4">Careers</h3>
+    <p class="MainText">my careers...</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Careers',
+};
+</script>
+
+<style scoped>
+.Careers .HeadingText {
+  font-size: 1.4rem;
+  font-weight: 300;
+  color: gray;
+}
+.Careers .MainText {
+  font-size: 1.2rem;
+  font-weight: 300;
+}
+</style>
--- src/components/PortfolioAbout/PortfolioAbout.vue
+++ src/components/PortfolioAbout/PortfolioAbout.vue
@@ -2,19 +2,22 @@
   <section class="PortfolioAbout">
     <h2 class="HeadingText text-center py-3">About</h2>
     <about-me class="mb-5" />
-    <skills />
+    <skills class="mb-5" />
+    <careers />
   </section>
 </template>
 
 <script>
 import AboutMe from './AboutMe/AboutMe';
 import Skills from './Skills/Skills';
+import Careers from './Careers/Careers';
 
 export default {
   name: 'PortflioAbout',
   components: {
     AboutMe,
     Skills,
+    Careers,
   },
 };
 </script>
