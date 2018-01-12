45626f13f3547f8e566b75d0d7a170c95a0037f0
KunhoLee <gunho1020@gmail.com>
2018-01-12 10:57:52 +0900

    About 페이지에 그림자 효과 추가

--- src/components/PortfolioAbout/AboutMe/AboutMe.vue
+++ src/components/PortfolioAbout/AboutMe/AboutMe.vue
@@ -12,6 +12,9 @@ export default {
 </script>
 
 <style scoped>
+.AboutMe {
+  box-shadow: 0 8px 38px rgba(133, 133, 133, 0.3), 0 5px 12px rgba(133, 133, 133,0.22);
+}
 .AboutMe .HeadingText {
   font-size: 1.4rem;
   font-weight: 300;
--- src/components/PortfolioAbout/Careers/Careers.vue
+++ src/components/PortfolioAbout/Careers/Careers.vue
@@ -12,6 +12,9 @@ export default {
 </script>
 
 <style scoped>
+.Careers {
+  box-shadow: 0 8px 38px rgba(133, 133, 133, 0.3), 0 5px 12px rgba(133, 133, 133,0.22);
+}
 .Careers .HeadingText {
   font-size: 1.4rem;
   font-weight: 300;
