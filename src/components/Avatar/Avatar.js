c88194ea774f122453a4d5c0cf241f25e93a1f96
KunhoLee <gunho1020@gmail.com>
2018-01-14 23:55:58 +0900

    init Avatar Component

---
+++ src/components/Avatar/Avatar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Avatar.css';
+
+function Avatar() {
+  return (
+    <div className="Portfolio_Avatar">This is Avatar component</div>
+  );
+}
+
+export default Avatar;
