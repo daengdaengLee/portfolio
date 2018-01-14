02fad1dc383c264f7d7bc901fbecbf0368fef07c
KunhoLee <gunho1020@gmail.com>
2018-01-15 00:08:03 +0900

    Avatar Component 작성

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -3,7 +3,13 @@ import './Avatar.css';
 
 function Avatar() {
   return (
-    <div className="Portfolio_Avatar">This is Avatar component</div>
+    <div className="Portfolio_Avatar">
+      <img 
+        src="https://user-images.githubusercontent.com/30795415/33804339-036ace2a-dde6-11e7-9b25-84e6c60be326.jpg"
+        alt="프로필 이미지"
+        className="Avatar_Image"
+      />
+    </div>
   );
 }
 
