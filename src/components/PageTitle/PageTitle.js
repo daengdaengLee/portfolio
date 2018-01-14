66937fe21ae3081bd702dc9f134522154e0f3daf
KunhoLee <gunho1020@gmail.com>
2018-01-15 00:12:23 +0900

    init PageTitle Component

---
+++ src/components/PageTitle/PageTitle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PageTitle.css';
+
+function PageTitle() {
+  return (
+    <h1 className="Portfolio_PageTitle">
+      This is PageTitle Component
+    </h1>
+  );
+}
+
+export default PageTitle;
