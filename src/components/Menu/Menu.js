c166cfd12df75e3aa271129054a800e33d4bf28c
KunhoLee <gunho1020@gmail.com>
2018-01-15 13:23:15 +0900

    Menu 컴포넌트 부트스트랩 적용

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,7 +3,7 @@ import './Menu.css';
 
 function Menu({ menuName }) {
   return (
-    <div className="Portfolio_Menu">
+    <div className="Portfolio_Menu rounded w-100 h-100 d-inline-flex justify-content-center align-items-center">
       {menuName}
     </div>
   );
