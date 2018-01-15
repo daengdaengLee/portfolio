d236ac687d659a66e763300bcbd2614d30b7e15d
KunhoLee <gunho1020@gmail.com>
2018-01-16 00:22:11 +0900

    init ContactList component

---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ContactList.css';
+
+function ContactList() {
+  return (
+    <div className="Portfolio_ContactList">ContactList component</div>
+  );
+}
+
+export default ContactList;
