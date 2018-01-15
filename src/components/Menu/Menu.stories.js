cbe783de757884758aa4792627f71b06909fb286
KunhoLee <gunho1020@gmail.com>
2018-01-15 12:39:23 +0900

    init Menu Component

---
+++ src/components/Menu/Menu.stories.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import Menu from './Menu';
+
+storiesOf('Menu', module)
+.addDecorator((story) => (
+  <div style={{ width: '5rem', height: '5rem', marginTop: '3rem', marginLeft: '3rem'}}>
+    {story()}
+  </div>
+))
+.add('Menu Component Projects', () => <Menu
+  
+/>);
