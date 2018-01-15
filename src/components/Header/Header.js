0ef8b6225bacb1526670b966a1c13f12aee9d086
KunhoLee <gunho1020@gmail.com>
2018-01-15 13:27:33 +0900

    init Header Component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="Portfolio_Header">header component</div>
+  );
+}
+
+export default Header;
