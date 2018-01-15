ac9c78a9ad950c46bab8a165ca23bdd6a1d2c1eb
KunhoLee <gunho1020@gmail.com>
2018-01-16 01:23:25 +0900

    ContactList 컴포넌트 작성

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,9 +1,48 @@
 import React from 'react';
-import './ContactList.css';
+
+import ContactButton from './ContactButton/ContactButton';
+
+const contactInfo = [
+  {
+    id: 1,
+    tagName: 'FaGithubAlt',
+    iconDesc: 'github icon',
+    linkURL: 'https://github.com/daengdaengLee',
+  },
+  {
+    id: 2,
+    tagName: 'FaFacebook',
+    iconDesc: 'facebook icon',
+    linkURL: 'https://www.facebook.com/gunho.lee.566',
+  },
+  {
+    id: 3,
+    tagName: 'FaCodepen',
+    iconDesc: 'codepen icon',
+    linkURL: 'https://codepen.io/daengdaengLee/',
+  },
+  {
+    id: 4,
+    tagName: 'FaEnvelopeO',
+    iconDesc: 'codepen icon',
+    linkURL: 'mailto: gunho1020@gmail.com',
+  },
+];
 
 function ContactList() {
   return (
-    <div className="Portfolio_ContactList">ContactList component</div>
+    <ul className="list-inline">
+      {contactInfo.map((info) => (
+        <li className="list-inline-item mx-2">
+          <ContactButton
+            key={info.id}
+            tagName={info.tagName}
+            iconDesc={info.iconDesc}
+            linkURL={info.linkURL}
+          />
+        </li>
+      ))}
+    </ul>
   );
 }
 
