b006986b332492f5fccb8866fa9cf84ade39b6f7
KunhoLee <gunho1020@gmail.com>
2018-01-15 13:09:34 +0900

    Avatar component 부트스트랩 적용

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -3,11 +3,11 @@ import './Avatar.css';
 
 function Avatar() {
   return (
-    <div className="Portfolio_Avatar">
+    <div className="Portfolio_Avatar rounded-circle p-2">
       <img 
         src="https://user-images.githubusercontent.com/30795415/33804339-036ace2a-dde6-11e7-9b25-84e6c60be326.jpg"
         alt="프로필 이미지"
-        className="Avatar_Image"
+        className="Avatar_Image rounded-circle w-100"
       />
     </div>
   );
