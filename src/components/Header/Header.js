6d2aa9a52949c0f712278a8f9a729fc298bb676b
KunhoLee <gunho1020@gmail.com>
2018-01-16 01:49:07 +0900

    Header컴포넌트에 필요한 컴포넌트 import

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,20 @@
 import React from 'react';
+
+import Avatar from './Avatar/Avatar';
+import ContactList from './ContactList/ContactList';
+import MenuBar from './MenuBar/MenuBar';
+import PageTitle from './PageTitle/PageTitle';
+
 import './Header.css';
 
 function Header() {
   return (
-    <div className="Portfolio_Header">header component</div>
+    <header className="Portfolio_Header">
+      <Avatar />
+      <ContactList />
+      <MenuBar />
+      <PageTitle />
+    </header>
   );
 }
 
--- src/components/Header/Header.stories.js
+++ src/components/Header/Header.stories.js
@@ -6,4 +6,10 @@ import { linkTo } from '@storybook/addon-links';
 
 import Header from './Header';
 
-storiesOf('Header', module).add('Header Component', () => <Header />);
+storiesOf('Header', module)
+.addDecorator((story) => (
+  <div className="container">
+    {story()}
+  </div>
+))
+.add('Header Component', () => <Header />);
