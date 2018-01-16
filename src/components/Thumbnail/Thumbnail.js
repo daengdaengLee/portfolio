64bf1376461ec607c7c11bd2d88af5483dbc4ba1
KunhoLee <gunho1020@gmail.com>
2018-01-16 11:10:31 +0900

    init Thumbnail Component

---
+++ src/components/Thumbnail/Thumbnail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Thumbnail.css';
+
+function Thumbnail() {
+  return (
+    <div className="Portfolio_Thumbnail">
+      test
+    </div>
+  );
+}
+
+export default Thumbnail;
