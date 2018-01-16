ab8ca7d65e6282fae5172f1811e8f6486fcabc19
KunhoLee <gunho1020@gmail.com>
2018-01-16 11:57:20 +0900

    init ProjectDesc Component

---
+++ src/components/ProjectDesc/ProjectDesc.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ProjectDesc.css';
+
+function ProjectDesc() {
+  return (
+    <div className="Portfolio_ProjectDesc">
+      test
+    </div>
+  );
+}
+
+export default ProjectDesc;
