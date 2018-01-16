44c8a01730ac387623cd2f7018447186d7e391cf
KunhoLee <gunho1020@gmail.com>
2018-01-16 10:44:46 +0900

    inti Footer Component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className="Portfolio_Footer">
+      test
+    </div>
+  );
+}
+
+export default Footer;
