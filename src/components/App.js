65fe161a988b496029ca4d48fd62d94cf19337bf
KunhoLee <gunho1020@gmail.com>
2018-01-16 11:04:31 +0900

    App컴포넌트에 Footer컴포넌트 import

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Header from './Header/Header';
+import Footer from './Footer/Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App container">
         <Header />
+        <Footer />
       </div>
     );
   }
