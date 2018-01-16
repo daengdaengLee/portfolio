9962a7e155de299e39fb864559c2c512252e5544
KunhoLee <gunho1020@gmail.com>
2018-01-16 11:41:13 +0900

    init ProjectSynopsis Component

---
+++ src/components/ProjectSynopsis/ProjectSynopsis.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ProjectSynopsis.css';
+
+function ProjectSynopsis() {
+  return (
+    <div className="Portfolio_ProjectSynopsis">
+      test
+    </div>
+  );
+}
+
+export default ProjectSynopsis;
