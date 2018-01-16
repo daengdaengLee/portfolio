c33880ef1709f03c576377099399c9b0ba97ebb7
KunhoLee <gunho1020@gmail.com>
2018-01-16 12:13:06 +0900

    init ProjectCard Component

---
+++ src/components/ProjectCard/ProjectCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ProjectCard.css';
+
+function ProjectCard() {
+  return (
+    <section className="Portfolio_ProjectCard">
+      test
+    </section>
+  );
+}
+
+export default ProjectCard;
