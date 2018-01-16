3ed897ba8f0f9ad647de4a45bd318dbee4f3e942
KunhoLee <gunho1020@gmail.com>
2018-01-16 12:06:45 +0900

    PortfolioDesc 컴포넌트 작성

--- src/components/ProjectDesc/ProjectDesc.js
+++ src/components/ProjectDesc/ProjectDesc.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import ProjectTitle from './ProjectTitle/ProjectTitle';
+import ProjectSynopsis from './ProjectSynopsis/ProjectSynopsis';
 import './ProjectDesc.css';
 
-function ProjectDesc() {
+function ProjectDesc({ title, desc }) {
   return (
-    <div className="Portfolio_ProjectDesc">
-      test
+    <div className="Portfolio_ProjectDesc p-4">
+      <ProjectTitle
+        projectTitle={title}
+      />
+      <ProjectSynopsis
+        projectDesc={desc}
+      />
     </div>
   );
 }
+
+ProjectDesc.propTypes = {
+  title: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
+};
 
 export default ProjectDesc;
