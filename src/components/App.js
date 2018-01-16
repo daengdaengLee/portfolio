00bc8d28bab98abe2b55fcd986cea5af962d210f
KunhoLee <gunho1020@gmail.com>
2018-01-16 10:37:32 +0900

    App컴포넌트에 Header컴포넌트 import

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Avatar from './Avatar/Avatar';
-import PageTitle from './PageTitle/PageTitle';
+import Header from './Header/Header';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Avatar />
-        <PageTitle />
+      <div className="App container">
+        <Header />
       </div>
     );
   }
