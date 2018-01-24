d86826f714e9001be13f65769ece7e26ca80dc2d
KunhoLee <gunho1020@gmail.com>
2018-01-24 13:12:56 +0900

    Footer컴포넌트: footerInfo 주입
    - footerInfo.js:
      - 생성
      - userLink, codeRepoLink 정보 작성
    
    - Footer.js:
      - prop-types import 구문 삭제
      - footerInfo.js import
      - userLink, codeRepoLink props 삭제
      - footerInfo에서 userLink, codeRepoLink 정보 얻어와서 a 태그의 href속성에 부여
      - propTypes 체크 코드 삭제

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+
+import footerInfo from './footerInfo';
+
 import './Footer.css';
 
-function Footer({ userLink, codeRepoLink }) {
+function Footer() {
   return (
     <footer className="Portfolio_Footer pb-5">
       <p className="FooterText text-center">
-        Coded by <a href={userLink} target="_blank" className="FooterLink">KunhoLee</a>.<br />
-        You can see the code from <a href={codeRepoLink} target="_blank" className="FooterLink">here</a>!
+        Coded by <a href={footerInfo.userLink} target="_blank" className="FooterLink">KunhoLee</a>.<br />
+        You can see the code from <a href={footerInfo.codeRepoLink} target="_blank" className="FooterLink">here</a>!
       </p>
     </footer>
   );
 }
-
-Footer.propTypes = {
-  userLink: PropTypes.string.isRequired,
-  codeRepoLink: PropTypes.string.isRequired,
-};
 
 export default Footer;
