668bd1e3bded4fade917932a84ece5176682fa43
KunhoLee <gunho1020@gmail.com>
2018-01-24 12:49:51 +0900

    App컴포넌트: ProjectPage컴포넌트 테스트
    - App.js:
      - App.css import 구문 삭제
      - ProjectPage 컴포넌트 import
      - ProjectPage 컴포넌트 rendering
      - 각 영역에 wrapper div 태그 작성해서 margin값 조정

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-import './App.css';
 
 import Header from './Header/Header';
+import ProjectPage from './ProjectPage/ProjectPage';
 import Footer from './Footer/Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App container">
-        <Header />
-        <Footer />
+        <div className="mt-5">
+          <Header />
+        </div>
+        <div className="my-5">
+          <ProjectPage />
+        </div>
+        <div className="mb-5">
+          <Footer />
+        </div>
       </div>
     );
   }
