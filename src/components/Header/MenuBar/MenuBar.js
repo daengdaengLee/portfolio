8b2a338a74556f7817c37cec387e3b1cb6fe1203
KunhoLee <gunho1020@gmail.com>
2018-01-24 13:42:41 +0900

    MenuBar컴포넌트: Blog태그에 링크 추가, css로 text-decoration관리
    - MenuBar.css:
      - link클래스에 text-decoration: none 부여
    
    - MenuBar.js:
      - MenuBar.css import
      - ul 태그에 Portfolio_MenuBar 클래스 부여
      - Projects 메뉴의 Link 컴포넌트에 link 클래스 부여
      - Blog 메뉴에 a 태그 작성, 블로그로 링크 걸어둠

--- src/components/Header/MenuBar/MenuBar.js
+++ src/components/Header/MenuBar/MenuBar.js
@@ -4,17 +4,19 @@ import { Link } from 'react-router-dom';
 
 import Menu from './Menu/Menu';
 
+import './MenuBar.css';
+
 function MenuBar() {
   return (
-    <ul className="row justify-content-center w-100 mx-0">
+    <ul className="Portfolio_MenuBar row justify-content-center w-100 mx-0">
       <li className="col-3 col-md-2">
-        <Link to="/portfolio" style={{ textDecoration: 'none' }}><Menu menuName="Projects" /></Link>
+        <Link to="/portfolio" className="link"><Menu menuName="Projects" /></Link>
       </li>
       <li className="col-3 col-md-2">
         <Menu menuName="About" />
       </li>
       <li className="col-3 col-md-2">
-        <Menu menuName="Blog" />
+        <a href="https://daengdaenglee.github.io/" target="_blank" rel="noopener noreferrer" className="link"><Menu menuName="Blog" /></a>
       </li>
     </ul>
   );
