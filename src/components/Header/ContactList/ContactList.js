c25e25fe3d27ea01c76f0057154afc22c876ceb7
KunhoLee <gunho1020@gmail.com>
2018-01-24 13:18:00 +0900

    ContactList컴포넌트: contactInfo 분리
    - contactInfo.js:
      - 생성
      - 기존 contactInfo 배열 복사
    
    - ContactList.js:
      - contactInfo.js import

--- src/components/Header/ContactList/ContactList.js
+++ src/components/Header/ContactList/ContactList.js
@@ -2,32 +2,7 @@ import React from 'react';
 
 import ContactButton from './ContactButton/ContactButton';
 
-const contactInfo = [
-  {
-    id: 1,
-    tagName: 'FaGithubAlt',
-    iconDesc: 'github icon',
-    linkURL: 'https://github.com/daengdaengLee',
-  },
-  {
-    id: 2,
-    tagName: 'FaFacebook',
-    iconDesc: 'facebook icon',
-    linkURL: 'https://www.facebook.com/gunho.lee.566',
-  },
-  {
-    id: 3,
-    tagName: 'FaCodepen',
-    iconDesc: 'codepen icon',
-    linkURL: 'https://codepen.io/daengdaengLee/',
-  },
-  {
-    id: 4,
-    tagName: 'FaEnvelopeO',
-    iconDesc: 'codepen icon',
-    linkURL: 'mailto: gunho1020@gmail.com',
-  },
-];
+import contactInfo from './contactInfo';
 
 function ContactList() {
   return (
