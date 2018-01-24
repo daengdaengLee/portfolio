57623ae7a1a44ce85841eb58a358e6ca216ec412
KunhoLee <gunho1020@gmail.com>
2018-01-24 12:13:54 +0900

    ProjectPage컴포넌트: prjectInfo prop 전달
    - ProjectPage.js:
      - projectInfo prop 전달
      - prop-types 체크
      - JSON.stringify()로 출력 확인
    
    - projectInfo.js:
      - 생성, 임시 작성
    
    - ProjectPage.stories.js:
      - projectInfo.js import
      - ProjectPage컴포넌트의 projectInfo prop으로 projectInfo.js내용 전달

--- src/components/ProjectPage/ProjectPage.js
+++ src/components/ProjectPage/ProjectPage.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import ProjectCard from './ProjectCard/ProjectCard';
 
-function ProjectPage() {
+function ProjectPage({ projectInfo }) {
   return (
     <section>
       <h2>Projects</h2>
+      {JSON.stringify(projectInfo)}
     </section>
   );
 }
+
+ProjectPage.propTypes = {
+  projectInfo: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    imgURL: PropTypes.string.isRequired,
+    altText: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    desc: PropTypes.string.isRequired,
+  })).isRequired
+};
 
 export default ProjectPage;
