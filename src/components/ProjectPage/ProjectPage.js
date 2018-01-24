69f2ba97b339c8aa0915139692f777e5853062d1
KunhoLee <gunho1020@gmail.com>
2018-01-24 12:41:18 +0900

    ProjectPage컴포넌트: Projects 제목 스타일링
    - ProjectPage.js:
      - 컴포넌트 root section 태그에 Portfolio_ProjectPage class 부여
      - h2 태그에 headingText, text-center, py-4 class 부여
    
    - ProjectPage.css:
      - 생성
      - .headingText에 font-size: 2rem; 부여

--- src/components/ProjectPage/ProjectPage.js
+++ src/components/ProjectPage/ProjectPage.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ProjectCard from './ProjectCard/ProjectCard';
 import projectInfo from './projectInfo';
 
+import './ProjectPage.css';
+
 function ProjectPage() {
   return (
-    <section>
-      <h2>Projects</h2>
+    <section className="Portfolio_ProjectPage">
+      <h2 className="headingText text-center py-4">Projects</h2>
       <div className="row mx-0">
         {projectInfo.map(project => (
           <div className="col-md-6 my-5">
