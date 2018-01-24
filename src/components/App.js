a9fd0a0beea7959a3445c336ba038edd72064ff9
KunhoLee <gunho1020@gmail.com>
2018-01-24 13:23:24 +0900

    App컴포넌트: react router import
    - App.js:
      - react-router-dom패키지에서 BrowserRouter import
      - 최상단 루트로 BrowserRouter 컴포넌트 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import Header from './Header/Header';
 import ProjectPage from './ProjectPage/ProjectPage';
@@ -7,17 +8,19 @@ import Footer from './Footer/Footer';
 class App extends Component {
   render() {
     return (
-      <div className="App container">
-        <div className="mt-5">
-          <Header />
+      <BrowserRouter>
+        <div className="App container">
+          <div className="mt-5">
+            <Header />
+          </div>
+          <div className="my-5">
+            <ProjectPage />
+          </div>
+          <div className="mb-5">
+            <Footer />
+          </div>
         </div>
-        <div className="my-5">
-          <ProjectPage />
-        </div>
-        <div className="mb-5">
-          <Footer />
-        </div>
-      </div>
+      </BrowserRouter>
     );
   }
 }
