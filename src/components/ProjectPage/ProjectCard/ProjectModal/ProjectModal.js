b72df92717820943650ad54bec58b9831fb23a29
KunhoLee <gunho1020@gmail.com>
2018-01-25 22:56:20 +0900

    ProjectModal컴포넌트: ProjectModalContent컴포넌트 주입
    - ProjectModal.js:
      - ProjectModalContent 컴포넌트 import
      - imgURL, altText, title, desc, pageLink, repoLink props 생성
      - ProjectModalContent 컴포넌트 생성
      - imgURL, altText, title, desc, pageLink, repoLink  props ProjectModalContent 컴포넌트에 전달
      - imgURL, altText, title, desc, pageLink, repoLink props 타입 검사
    
    - ProjectModal.stories.js:
      - 불필요한 decorator 주석 코드 삭제
      - dummy imgURL, altText, title, desc, pageLink, repoLink props 전달

--- src/components/ProjectPage/ProjectCard/ProjectModal/ProjectModal.js
+++ src/components/ProjectPage/ProjectCard/ProjectModal/ProjectModal.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactModal from 'react-modal';
 import PropTypes from 'prop-types';
+import ProjectModalContent from './ProjectModalContent/ProjectModalContent';
 
-function ProjectModal({ isOpen, contentLabel }) {
+function ProjectModal({ isOpen, contentLabel, imgURL, altText, title, desc, pageLink, repoLink }) {
   return (
     <ReactModal
       isOpen={isOpen}
       contentLabel={contentLabel}
     >
-      ProjectModal Component
+      <ProjectModalContent
+        imgURL={imgURL}
+        altText={altText}
+        title={title}
+        desc={desc}
+        pageLink={pageLink}
+        repoLink={repoLink}
+      />
     </ReactModal>
   );
 }
@@ -16,6 +24,12 @@ function ProjectModal({ isOpen, contentLabel }) {
 ProjectModal.propTypes = {
   isOpen: PropTypes.bool.isRequired,
   contentLabel: PropTypes.string.isRequired,
+  imgURL: PropTypes.string.isRequired,
+  altText: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
+  pageLink: PropTypes.string.isRequired,
+  repoLink: PropTypes.string.isRequired,
 };
 
 export default ProjectModal;
