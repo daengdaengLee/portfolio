c721318dce29c4af35f98887674b4c56e80b1719
KunhoLee <gunho1020@gmail.com>
2018-01-25 21:18:35 +0900

    ProjectModalContent컴포넌트: ProjectModalDesc컴포넌트 주입
    - ProjectModalContent.js:
      - ProjectModalDesc 컴포넌트 import
      - title, desc, pageLink, repoLink props 생성
      - ProjectModalDesc컴포넌트 작성
      - ProjectModalDesc컴포넌트에 title, desc, pageLink, repoLink props 전달
      - title, desc, pageLink, repoLink props 타입 체크
    
    - ProjectModalContent.stories.js:
      - dummyText 생성
      - dummy title, desc, repoLink, pageLink props 전달

--- src/components/ProjectPage/ProjectModal/ProjectModalContent/ProjectModalContent.js
+++ src/components/ProjectPage/ProjectModal/ProjectModalContent/ProjectModalContent.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Thumbnail from '../../ProjectCard/Thumbnail/Thumbnail';
+import ProjectModalDesc from './ProjectModalDesc/ProjectModalDesc';
 
-function ProjectModalContent({ imgURL, altText }) {
+function ProjectModalContent({ imgURL, altText, title, desc, pageLink, repoLink }) {
   return (
     <section>
       <Thumbnail
         imgURL={imgURL}
         altText={altText}
+      />
+      <ProjectModalDesc
+        title={title}
+        desc={desc}
+        pageLink={pageLink}
+        repoLink={repoLink}
       />
     </section>
   );
@@ -16,6 +23,10 @@ function ProjectModalContent({ imgURL, altText }) {
 ProjectModalContent.propTypes = {
   imgURL: PropTypes.string.isRequired,
   altText: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
+  pageLink: PropTypes.string.isRequired,
+  repoLink: PropTypes.string.isRequired,
 };
 
 export default ProjectModalContent;
