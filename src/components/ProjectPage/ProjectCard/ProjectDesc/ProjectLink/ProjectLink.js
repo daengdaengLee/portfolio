28d5d0e95aa82f13965f753c03a6b68e794c51c2
KunhoLee <gunho1020@gmail.com>
2018-01-25 12:37:58 +0900

    ProjectLink컴포넌트: 스타일 적용
    - ProjectLink.js:
      - ul태그에 Portfolio_ProjectLink 클래스 부여
      - a 태그에 link 클래스 부여
    
    - ProjectLink.css
      - link클래스에 text-decoration: none; color: gray;
      - link클래스 hover시 opacity: 0.7;

--- src/components/ProjectPage/ProjectCard/ProjectDesc/ProjectLink/ProjectLink.js
+++ src/components/ProjectPage/ProjectCard/ProjectDesc/ProjectLink/ProjectLink.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import './ProjectLink.css';
+
 function ProjectLink() {
   return (
-    <ul>
-      <li className="my-1"><a href="#" target="_blank">Project Page</a></li>
-      <li><a href="#" target="_blank">Project Repo</a></li>
+    <ul className="Portfolio_ProjectLink">
+      <li className="my-1"><a href="#" target="_blank" className="link">Project Page</a></li>
+      <li><a href="#" target="_blank" className="link">Project Repo</a></li>
     </ul>
   );
 }
