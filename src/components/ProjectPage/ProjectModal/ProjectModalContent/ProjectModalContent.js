db99c85d2958dea424df4880fee6686621d1012e
KunhoLee <gunho1020@gmail.com>
2018-01-25 19:48:13 +0900

    ProjectModalContent컴포넌트: Thumbnail컴포넌트 주입
    - ProjectModalContent.js:
      - import prop-types
      - import Thumbnail Component
      - make imgURL, altText props
      - div태그 section태그로 변경
      - section태그 안에 Thumbnail컴포넌트 작성, imgURL, altText 전달
      - imgURL, altText propTypes 체크
    
    - ProjectModalContent.stories.js:
      - Decorator 추가
      - dummy imgURL, altText 전달

--- src/components/ProjectPage/ProjectModal/ProjectModalContent/ProjectModalContent.js
+++ src/components/ProjectPage/ProjectModal/ProjectModalContent/ProjectModalContent.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Thumbnail from '../../ProjectCard/Thumbnail/Thumbnail';
 
-function ProjectModalContent() {
+function ProjectModalContent({ imgURL, altText }) {
   return (
-    <div>
-      ProjectModalContent Component
-    </div>
+    <section>
+      <Thumbnail
+        imgURL={imgURL}
+        altText={altText}
+      />
+    </section>
   );
 }
+
+ProjectModalContent.propTypes = {
+  imgURL: PropTypes.string.isRequired,
+  altText: PropTypes.string.isRequired,
+};
 
 export default ProjectModalContent;
--- src/components/ProjectPage/ProjectModal/ProjectModalContent/ProjectModalContent.stories.js
+++ src/components/ProjectPage/ProjectModal/ProjectModalContent/ProjectModalContent.stories.js
@@ -7,9 +7,12 @@ import { linkTo } from '@storybook/addon-links';
 import ProjectModalContent from './ProjectModalContent';
 
 storiesOf('ProjectModalContent', module)
-// .addDecorator((story) => (
-//   <div className="container" style={{ margin: '2rem' }}>
-//     {story()}
-//   </div>
-// ))
-.add('ProjectModalContent Component', () => <ProjectModalContent />);
+.addDecorator((story) => (
+  <div style={{ width: '24rem', margin: '5rem' }}>
+    {story()}
+  </div>
+))
+.add('ProjectModalContent Component', () => <ProjectModalContent
+  imgURL="https://user-images.githubusercontent.com/30795415/33804319-b2564816-dde5-11e7-8d4a-60fc12d68898.jpg"
+  altText="Web Calculator Thumbnail Image"
+/>);
