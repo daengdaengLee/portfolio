2fcfece8273e6866841600cfcef9c4d4e55e4087
KunhoLee <gunho1020@gmail.com>
2018-01-25 12:22:47 +0900

    ProjectLink컴포넌트: 링크 작성
    - ProjectLink.js:
      - Project Page 링크 작성
      - Project Repo 링크 작성
      - 두 링크를 ul태그 리스트로 표현
      - 두 링크 사이에 0.25rem 간격 둠

--- src/components/ProjectPage/ProjectCard/ProjectDesc/ProjectLink/ProjectLink.js
+++ src/components/ProjectPage/ProjectCard/ProjectDesc/ProjectLink/ProjectLink.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 function ProjectLink() {
   return (
-    <div>
-      ProjectLink Component
-    </div>
+    <ul>
+      <li className="my-1"><a href="#" target="_blank">Project Page</a></li>
+      <li><a href="#" target="_blank">Project Repo</a></li>
+    </ul>
   );
 }
 
