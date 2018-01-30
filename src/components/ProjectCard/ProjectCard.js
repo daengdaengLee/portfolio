b35fa9cea58005a4de69ebda4b8f932a59514d3b
KunhoLee <gunho1020@gmail.com>
2018-01-30 11:24:48 +0900

    Thumbnail컴포넌트: 디렉토리 위치 변경
    - components폴더 바로 아래로 위치 변경

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ProjectDesc from './ProjectDesc/ProjectDesc';
-import Thumbnail from './Thumbnail/Thumbnail';
+import Thumbnail from '../Thumbnail/Thumbnail';
 import ProjectModal from './ProjectModal/ProjectModal';
 import './ProjectCard.css';
 
