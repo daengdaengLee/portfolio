332abc45814f463b56b75f1a5c61d7e065fa6c11
KunhoLee <gunho1020@gmail.com>
2018-01-31 14:02:01 +0900

    AboutSkills컴포넌트: 레이아웃, 스타일링 작업

--- src/components/AboutSkills/index.js
+++ src/components/AboutSkills/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 
 import DefinitionList from '../DefinitionList';
 import listInfo from './listInfo';
+import './AboutSkills.css';
 
 function AboutSkills() {
   return (
-    <section className="p-4">
-      <h3 class="mb-4">Skills</h3>
-      <DefinitionList listInfo={listInfo} />
+    <section className="Portfolio_AboutSkills p-4">
+      <h3 class="heading text-center mb-4">Skills</h3>
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="w-75">
+          <DefinitionList listInfo={listInfo} />
+        </div>
+      </div>
     </section>
   );
 }
