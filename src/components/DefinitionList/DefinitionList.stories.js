00c899b96e77b64102ad494747084b0776dd2808
KunhoLee <gunho1020@gmail.com>
2018-01-31 13:46:26 +0900

    DefinitionList컴포넌트: 본문 코드 작성
    - index.js:
      - propTypes 패키지 import
      - class 형식으로 전환
      - listInfo prop 받아서 재귀적으로 랜더링
    
    - DefinitionList.stories.js:
      - dummy list 주입

--- src/components/DefinitionList/DefinitionList.stories.js
+++ src/components/DefinitionList/DefinitionList.stories.js
@@ -6,10 +6,97 @@ import { linkTo } from '@storybook/addon-links';
 
 import DefinitionList from './index';
 
+const dummyList = [
+  {
+    id: 1,
+    listTerm: '1st - 1depth',
+    listDesc: [
+      {
+        id: 1,
+        listTerm: '1st - 2depth'
+      },
+      {
+        id: 2,
+        listTerm: '2nd - 2depth'
+      },
+      {
+        id: 3,
+        listTerm: '3rd - 2depth',
+        listDesc: [
+          {
+            id: 1,
+            listTerm: '1st - 3depth',
+          },
+          {
+            id: 2,
+            listTerm: '2nd - 3depth',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    listTerm: '2nd - 1depth',
+    listDesc: [
+      {
+        id: 1,
+        listTerm: '1st - 2depth'
+      },
+      {
+        id: 2,
+        listTerm: '2nd - 2depth'
+      },
+      {
+        id: 3,
+        listTerm: '3rd - 2depth',
+        listDesc: [
+          {
+            id: 1,
+            listTerm: '1st - 3depth',
+          },
+          {
+            id: 2,
+            listTerm: '2nd - 3depth',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 3,
+    listTerm: '3rd - 1depth',
+    listDesc: [
+      {
+        id: 1,
+        listTerm: '1st - 2depth'
+      },
+      {
+        id: 2,
+        listTerm: '2nd - 2depth'
+      },
+      {
+        id: 3,
+        listTerm: '3rd - 2depth',
+        listDesc: [
+          {
+            id: 1,
+            listTerm: '1st - 3depth',
+          },
+          {
+            id: 2,
+            listTerm: '2nd - 3depth',
+          },
+        ],
+      },
+    ],
+  },
+];
+
 storiesOf('DefinitionList', module)
 .addDecorator((story) => (
   <div className="container">
     {story()}
   </div>
 ))
-.add('DefinitionList Component', () => <DefinitionList />);
+.add('DefinitionList Component', () => <DefinitionList listInfo={dummyList} />);
