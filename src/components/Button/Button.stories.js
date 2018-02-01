8f934cdea36dd7b2e041735bcbc9a4b61ff53158
KunhoLee <gunho1020@gmail.com>
2018-02-01 19:42:54 +0900

    MenuButton컴포넌트 이름 Button으로 변경

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -4,20 +4,20 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import MenuButton from './index';
+import Button from './index';
 
-storiesOf('MenuButton', module)
+storiesOf('Button', module)
 .addDecorator((story) => (
   <div style={{ width: '8rem', height: '4rem', marginTop: '3rem', marginLeft: '3rem'}}>
     {story()}
   </div>
 ))
-.add('MenuButton Component Projects', () => <MenuButton
-  menuName="Projects"
+.add('Button Component Projects', () => <Button
+  buttonName="Projects"
 />)
-.add('MenuButton Component About', () => <MenuButton
-  menuName="About"
+.add('Button Component About', () => <Button
+  buttonName="About"
 />)
-.add('MenuButton Component Blog', () => <MenuButton
-  menuName="Blog"
+.add('Button Component Blog', () => <Button
+  buttonName="Blog"
 />);
