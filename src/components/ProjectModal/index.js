408811537705a1d0567bdf28558d1f873c298d9a
KunhoLee <gunho1020@gmail.com>
2018-02-02 12:28:26 +0900

    Modal창 스크롤 부트스트랩처럼 표현

--- src/components/ProjectModal/index.js
+++ src/components/ProjectModal/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactModal from 'react-modal';
 import PropTypes from 'prop-types';
 
@@ -9,30 +9,52 @@ import './ProjectModal.css';
 
 ReactModal.setAppElement('#root');
 
-function ProjectModal({ isOpen, contentLabel, onRequestClose, imgURL, altText, title, desc, pageLink, repoLink }) {
-  return (
-    <ReactModal
-      isOpen={isOpen}
-      contentLabel={contentLabel}
-      onRequestClose={onRequestClose}
-      bodyOpenClassName="modal-open"
-      className="Portfolio_ProjectModal_Content"
-    >
-      <ProjectModalContent
-        imgURL={imgURL}
-        altText={altText}
-        title={title}
-        desc={desc}
-        pageLink={pageLink}
-        repoLink={repoLink}
-      />
-      <div className="d-flex justify-content-center my-3 my-md-5">
-        <div className="w-50" onClick={onRequestClose}>
-          <Button buttonName="Close" />
+class ProjectModal extends Component {
+  
+  componentDidUpdate() {
+    const { isOpen } = this.props;
+    const overlay = document.querySelector('.Portfolio_ProjectModal_Overlay');
+    if(isOpen) overlay.scrollTop = 0;
+  }
+
+  render() {
+    const {
+      isOpen,
+      contentLabel,
+      onRequestClose,
+      imgURL,
+      altText,
+      title,
+      desc,
+      pageLink,
+      repoLink,
+    } = this.props;
+    return (
+      <ReactModal
+        isOpen={isOpen}
+        contentLabel={contentLabel}
+        onRequestClose={onRequestClose}
+        bodyOpenClassName="modal-open"
+        overlayClassName="Portfolio_ProjectModal_Overlay"
+        className="Portfolio_ProjectModal_Content"
+      >
+        <ProjectModalContent
+          imgURL={imgURL}
+          altText={altText}
+          title={title}
+          desc={desc}
+          pageLink={pageLink}
+          repoLink={repoLink}
+        />
+        <div className="d-flex justify-content-center my-3 my-md-5">
+          <div className="w-50" onClick={onRequestClose}>
+            <Button buttonName="Close" />
+          </div>
         </div>
-      </div>
-    </ReactModal>
-  );
+      </ReactModal>
+    );
+  }
+  
 }
 
 ProjectModal.propTypes = {
