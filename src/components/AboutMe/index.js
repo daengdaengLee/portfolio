c09c1653e7bd72a421c8e87ac72b48d80c266e05
KunhoLee <gunho1020@gmail.com>
2018-02-02 13:21:20 +0900

    AboutMe컴포넌트에 Card컴포넌트 적용

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,23 +1,20 @@
 import React from 'react';
 
+import Card from '../Card';
 import Image from '../Image';
 import AboutMeText from '../AboutMeText';
 
+const left = (
+  <Image
+    src="https://user-images.githubusercontent.com/30795415/35600809-30b8806a-0673-11e8-984e-a259f5aa43e4.jpg"
+    altText="프로필 이미지"
+    rounded="rounded"
+  />
+);
+const right = <AboutMeText />;
+
 function AboutMe() {
-  return (
-    <div className="row mx-0">
-      <div className="col-md-4 p-4 d-inline-flex justify-content-center align-items-center">
-        <Image
-          src="https://user-images.githubusercontent.com/30795415/35600809-30b8806a-0673-11e8-984e-a259f5aa43e4.jpg"
-          altText="프로필 이미지"
-          rounded="rounded"
-        />
-      </div>
-      <div className="col-md-8 p-4 d-inline-flex justify-content-center align-items-center">
-        <AboutMeText />
-      </div>
-    </div>
-  );
+  return <Card left={left} right={right} />;
 }
 
 export default AboutMe;
