716a57ed886335ed3fad64322a0b10dab512b42e
KunhoLee <gunho1020@gmail.com>
2018-02-08 11:57:10 +0900

    CLN: TextButton컴포넌트 이욯해서 MenuBar 컴포넌트 리팩토링
    MenuBar/index.js 내부에서 MenuButton 컴포넌트를 정의하고 MenuBar 컴포넌트 내부에서 사용
    
    MenuButton 컴포넌트는 TextButton 컴포넌트 이용해 제작

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -2,21 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-import { Button } from '../';
+import { TextButton } from '../';
 
 import './MenuBar.css';
+
+function MenuButton({ menuName }) {
+  return (
+    <TextButton text={menuName} className="w-100" />
+  );
+}
 
 function MenuBar() {
   return (
     <ul className="Portfolio_MenuBar row justify-content-center w-100 mx-0">
       <li className="col-3 col-md-2">
-        <Link to="/portfolio" className="MenuBar_Link"><Button buttonName="Projects" /></Link>
+        <Link to="/portfolio" className="MenuBar_Link">
+          <MenuButton menuName="Projects" />
+        </Link>
       </li>
       <li className="col-3 col-md-2">
-        <Link to="/portfolio/about" className="MenuBar_Link"><Button buttonName="About" /></Link>
+        <Link to="/portfolio/about" className="MenuBar_Link">
+          <MenuButton menuName="About" />
+        </Link>
       </li>
       <li className="col-3 col-md-2">
-        <a href="https://daengdaenglee.github.io/" target="_blank" rel="noopener noreferrer" className="MenuBar_Link"><Button buttonName="Blog" /></a>
+        <a href="https://daengdaenglee.github.io/" target="_blank" rel="noopener noreferrer" className="MenuBar_Link">
+          <MenuButton menuName="Blog" />
+        </a>
       </li>
     </ul>
   );
