880c847e84b719b4d2272980e146ee797a74c3a7
KunhoLee <gunho1020@gmail.com>
2018-02-08 10:57:01 +0900

    CLN: Button 컴포넌트 기본적인 기능만 갖도록 축소

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -7,17 +7,4 @@ import { linkTo } from '@storybook/addon-links';
 import Button from './index';
 
 storiesOf('Button', module)
-.addDecorator((story) => (
-  <div style={{ width: '8rem', height: '4rem', marginTop: '3rem', marginLeft: '3rem'}}>
-    {story()}
-  </div>
-))
-.add('Button Component Projects', () => <Button
-  buttonName="Projects"
-/>)
-.add('Button Component About', () => <Button
-  buttonName="About"
-/>)
-.add('Button Component Blog', () => <Button
-  buttonName="Blog"
-/>);
+.add('Button Component', () => <Button>This is Button!</Button>);
