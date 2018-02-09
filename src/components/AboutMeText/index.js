ecaa3175b177c031ccb3584e81f8bc6db35dbd4a
KunhoLee <gunho1020@gmail.com>
2018-02-10 01:02:05 +0900

    ENH: AboutMeText 내용 수정
    이력서 내용과 동일하게 변경

--- src/components/AboutMeText/index.js
+++ src/components/AboutMeText/index.js
@@ -9,12 +9,15 @@ function AboutMeText() {
     <section className="Portfolio_AboutMeText">
       <Title tagName="h3" content="About Me" className="AboutMeText_Heading mb-4" />
       <p className="AboutMeText_Main">
-        새로운 기술을 배우는 것을 좋아하는 웹 프론트엔드 개발자입니다.
-        JavaScript와 JavaScript로 구현한 기술을 좋아합니다.
-        React.js, Vue.js 같은 웹 프론트엔드 프레임워크에 관심이 많습니다.
-        JavaScript 기반 백엔드 플랫폼인 Node.js 기술에도 흥미가 많습니다.
-        React-Native와 Android개발을 동시에 공부하면서 모바일 앱 개발을 공부하고 있습니다.
-        배운 것을 이용해 프로젝트 진행하기를 좋아합니다.
+        취미로 시작한 코딩에 푹 빠져버린 개발자입니다. 웹에서 동작하는 앱을 다룹니다.
+      </p>
+      <br />
+      <p className="AboutMeText_Main">
+        신기한 앱이 눈에 띄면 직접 만들어 봐야 직성이 풀립니다. 컴퓨터 공학이나 프로그래밍에 대한 전공 지식은 모르지만 만들고 싶은 앱을 만들기 위해 필요한 지식은 웹을 통해 폭넓게 수집합니다. 넘쳐나는 정보를 정리해서 큰 줄기를 보고 원리를 이해하려고 노력합니다.
+      </p>
+      <br />
+      <p className="AboutMeText_Main">
+        체력과 집중력이 좋아 매일 멈추지 않고 코딩할 수 있습니다.
       </p>
     </section>
   );
